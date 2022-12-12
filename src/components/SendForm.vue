5bdd7a85d49bb8908613616010e8d58d31a34d9a
PotatoHD404 <31506993+PotatoHD404@users.noreply.github.com>
2022-12-12 23:44:54 +0300

    Added project

--- src/components/SendForm.vue
+++ src/components/SendForm.vue
@@ -1,35 +1,32 @@
 <script lang="ts">
-import { getDatabase, ref, set, onValue, push } from "firebase/database";
-import type { Post } from '@/types/post';
-
-
+import {getDatabase, ref, set} from "firebase/database";
+import type {Post} from '@/types/post';
+// import { v4 as uuidv4 } from 'uuid';
 
 
 export default {
-  
-methods: {writePost: function () {
-    let text: Pick<Post, "text">;
-    // get text from input
-    text = {
-      text: this.text
-    }
-    let date = new Date();
-    let timestamp = date.getTime();
-    let db = getDatabase();
-    let postsRef = ref(db, 'posts/');
-    
-    // add new post to posts list
-    let newPostRef = push(postsRef);
-    set(newPostRef,{
-      text: text,
-      timestamp: timestamp
-    });
 
-    // clear input
-    this.text = "";
-    
+  methods: {
+    writePost: async function () {
+      let text: Pick<Post, "text">;
+      // get text from input
+      let date = new Date();
+      let timestamp = date.getTime();
+      let db = getDatabase();
+      let postsRef = ref(db, `posts/${(new Date()).getTime()}`);
+
+      // add new post to posts list
+      // let newPostRef = push(postsRef);
+      await set(postsRef, {
+            text: this.text,
+          }
+      );
+
+      // clear input
+      this.text = "";
+
+    },
   },
-},
   data() {
     return {
       text: "",
@@ -46,7 +43,7 @@ methods: {writePost: function () {
       <slot name="icon"></slot>
     </i>
     <input v-model="text"/>
-    <button @click="writePost()">Add 1</button>
+    <button @click="writePost()">Add</button>
     <div class="details">
       <h3>
         <slot name="heading"></slot>
@@ -57,74 +54,5 @@ methods: {writePost: function () {
 </template>
 
 <style scoped>
-.item {
-  margin-top: 2rem;
-  display: flex;
-}
 
-.details {
-  flex: 1;
-  margin-left: 1rem;
-}
-
-i {
-  display: flex;
-  place-items: center;
-  place-content: center;
-  width: 32px;
-  height: 32px;
-
-  color: var(--color-text);
-}
-
-h3 {
-  font-size: 1.2rem;
-  font-weight: 500;
-  margin-bottom: 0.4rem;
-  color: var(--color-heading);
-}
-
-@media (min-width: 1024px) {
-  .item {
-    margin-top: 0;
-    padding: 0.4rem 0 1rem calc(var(--section-gap) / 2);
-  }
-
-  i {
-    top: calc(50% - 25px);
-    left: -26px;
-    position: absolute;
-    border: 1px solid var(--color-border);
-    background: var(--color-background);
-    border-radius: 8px;
-    width: 50px;
-    height: 50px;
-  }
-
-  .item:before {
-    content: ' ';
-    border-left: 1px solid var(--color-border);
-    position: absolute;
-    left: 0;
-    bottom: calc(50% + 25px);
-    height: calc(50% - 25px);
-  }
-
-  .item:after {
-    content: ' ';
-    border-left: 1px solid var(--color-border);
-    position: absolute;
-    left: 0;
-    top: calc(50% + 25px);
-    height: calc(50% - 25px);
-  }
-
-  .item:first-of-type:before {
-    display: none;
-  }
-
-  .item:last-of-type:after {
-    display: none;
-  }
-}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,14 @@
-<script setup lang="ts">
+<script lang="ts">
 import Feed from '@/components/Feed.vue';
 import SendForm from '@/components/SendForm.vue';
+
+export default {
+  components: {
+    Feed,
+    SendForm
+  },
+}
+
 </script>
 
 <template>
